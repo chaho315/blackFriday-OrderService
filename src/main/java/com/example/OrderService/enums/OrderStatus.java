7a1e001874d875a24119b08ca66177c3ae89b84e
chaho510 <chaho510@naver.com>
2025-05-24 23:12:37 +0900

    EDA기반으로 리펙토링 진행

--- src/main/java/com/example/OrderService/enums/OrderStatus.java
+++ src/main/java/com/example/OrderService/enums/OrderStatus.java
@@ -2,6 +2,7 @@ package com.example.OrderService.enums;
 
 public enum OrderStatus {
     INITIATED,
+    PAYMENT_REQUESTED,
     PAYMENT_COMPLETED,
     DELIVERY_REQUESTED
 }
--- src/main/java/com/example/OrderService/service/OrderService.java
+++ src/main/java/com/example/OrderService/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.OrderService.service;
 
+import blackfriday.protobuf.EdaMessage;
 import com.example.OrderService.dto.*;
 import com.example.OrderService.entity.ProductOrder;
 import com.example.OrderService.enums.OrderStatus;
@@ -8,6 +9,7 @@ import com.example.OrderService.feign.DeliveryClient;
 import com.example.OrderService.feign.PaymentClient;
 import com.example.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,6 +28,9 @@ public class OrderService {
     @Autowired
     CatalogClient catalogClient;
 
+    @Autowired
+    private KafkaTemplate<String, byte[]> kafkaTemplate;
+
     public StartOrderResponseDto startOrder(Long userId, Long productId, Long count){
         //1. 상품 정보 조회
         var product = catalogClient.getProduct(productId);
@@ -42,6 +47,7 @@ public class OrderService {
                 productId,
                 count,
                 OrderStatus.INITIATED,
+                null,
                 null,
                 null
         );
@@ -62,16 +68,33 @@ public class OrderService {
         var product = catalogClient.getProduct(order.productId);
 
         //2. 결제
-        var processPaymentDto = new ProcessPaymentDto();
+       /* EDA기반으로 변경
+       var processPaymentDto = new ProcessPaymentDto();
         processPaymentDto.orderId = order.id;
         processPaymentDto.userId = order.userId;
         processPaymentDto.amountKRW = Long.parseLong(product.get("price").toString()) * order.count;
         processPaymentDto.paymentMethodId = paymentMethodId;
 
-        var payment = paymentClient.processPayment(processPaymentDto);
+        var payment = paymentClient.processPayment(processPaymentDto);*/
 
-        //3. 배송 요청
+        //2. 결제요청
+        var message = EdaMessage.PaymentRequestV1.newBuilder()
+                .setOrderId(order.id)
+                .setUserId(order.userId)
+                .setAmountKRW(Long.parseLong(product.get("price").toString()) * order.count)
+                .setPaymentMethodId(paymentMethodId)
+                .build();
+
+        kafkaTemplate.send("payment_request", message.toByteArray());
+
+        //3. 주문 정보 업데이트
         var address = deliveryClient.getAddress(addressId);
+        order.orderStatus = OrderStatus.PAYMENT_REQUESTED;
+        order.deliveryAddress = address.get("address").toString();
+        return orderRepository.save(order);
+        
+        //3. 배송 요청
+        /*var address = deliveryClient.getAddress(addressId);
 
         var processDeliveryDto = new ProcessDeliveryDto();
         processDeliveryDto.orderId = order.id;
@@ -85,14 +108,9 @@ public class OrderService {
         var decreaseStockCountDto = new DecreaseStockCountDto();
         decreaseStockCountDto.decreaseCount = order.count;
 
-        catalogClient.decreaseStockCount(order.productId, decreaseStockCountDto);
+        catalogClient.decreaseStockCount(order.productId, decreaseStockCountDto);*/
 
-        //5. 주문 정보 업데이트
-        order.paymentId = Long.parseLong(payment.get("id").toString());
-        order.deliveryId = Long.parseLong(delivery.get("id").toString());
-        order.orderStatus = OrderStatus.DELIVERY_REQUESTED;
 
-        return orderRepository.save(order);
     }
 
     public List<ProductOrder> getUserOrders(Long userId){
